b4a14c47a68dd01e66bec0892c5f55dcf5808fd9
Maxime AUBE <maxime.aube@protonmail.com>
2022-03-01 16:32:42 +0100

    tests ok pour construct, getters, setters

--- src/fr/maximob/color/model/Color.java
+++ src/fr/maximob/color/model/Color.java
@@ -1,4 +1,71 @@
 package fr.maximob.color.model;
 
+import java.util.Objects;
+
 public class Color {
+
+    protected int red;
+    protected int green;
+    protected int blue;
+
+    public Color(int red, int green, int blue) throws IllegalArgumentException {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public Color(String hexValue) throws IllegalArgumentException {
+        this.setHexValue(hexValue);
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public String getHexValue() {
+        return String.format("#%02X%02X%02X", this.red, this.green, this.blue);
+    }
+
+    public void setRed(int red) throws IllegalArgumentException {
+        this.red = red;
+    }
+
+    public void setGreen(int green) throws IllegalArgumentException {
+        this.green = green;
+    }
+
+    public void setBlue(int blue) throws IllegalArgumentException {
+        this.blue = blue;
+    }
+
+    public void setHexValue(String hexValue) throws IllegalArgumentException {
+        this.red = Integer.valueOf(hexValue.substring(1, 3), 16);
+        this.green = Integer.valueOf(hexValue.substring(3, 5), 16);
+        this.blue = Integer.valueOf(hexValue.substring(5, 7), 16);
+    }
+
+    public String toString() {
+        return "[value=" + this.getHexValue() + ", r=" + this.red + ", g=" + this.green + ", b=" + this.blue + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return red == color.red && green == color.green && blue == color.blue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(red, green, blue);
+    }
 }
--- test/fr/maximob/color/model/ColorTest.java
+++ test/fr/maximob/color/model/ColorTest.java
@@ -1,4 +1,206 @@
 package fr.maximob.color.model;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ColorTest {
+
+    protected int red = 213;
+    protected int green = 141;
+    protected int blue = 53;
+    protected String hexValue = this.getHexValue(this.red, this.green, this.blue);
+
+    protected Color color;
+
+    private String getHexValue(int r, int g, int b) {
+        return ("#" + Integer.toHexString(r) + Integer.toHexString(g) + Integer.toHexString(b)).toUpperCase();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        this.color = new Color(this.red, this.green, this.blue);
+    }
+
+
+    /*
+        Constructors
+     */
+
+    @Test
+    @Tags({
+        @Tag("construct"),
+        @Tag("equals")
+    })
+    public void testColorConstructRGBEquals() {
+        Color newColor = new Color(this.red, this.green, this.blue);
+        assertNotSame(this.color, newColor);
+        assertEquals(this.color, newColor);
+    }
+
+    @Test
+    @Tags({
+        @Tag("construct"),
+        @Tag("equals")
+    })
+    public void testColorConstructHexEquals() {
+        Color newColor = new Color(this.hexValue);
+        assertNotSame(this.color, newColor);
+        assertEquals(this.color, newColor);
+    }
+
+    // TODO => exceptions
+
+    @Test
+    @Tags({
+        @Tag("construct"),
+        @Tag("exception")
+    })
+    public void testColorConstructRGBException() {
+        Color newColor = new Color(this.red, this.green, this.blue);
+        assertNotSame(this.color, newColor);
+        assertEquals(this.color, newColor);
+    }
+
+    @Test
+    @Tags({
+        @Tag("construct"),
+        @Tag("exception")
+    })
+    public void testColorConstructHexException() {
+        Color newColor = new Color(this.hexValue);
+        assertNotSame(this.color, newColor);
+        assertEquals(this.color, new Color(this.hexValue));
+    }
+
+    /*
+        Getters
+     */
+
+    @Test
+    @Tags({
+        @Tag("getter"),
+        @Tag("equals")
+    })
+    public void testGetRedEquals() {
+        assertEquals(this.red, this.color.getRed());
+    }
+
+    @Test
+    @Tags({
+        @Tag("getter"),
+        @Tag("equals")
+    })
+    public void testGetGreenEquals() {
+        assertEquals(this.green, this.color.getGreen());
+    }
+
+    @Test
+    @Tags({
+        @Tag("getter"),
+        @Tag("equals")
+    })
+    public void testGetBlueEquals() {
+        assertEquals(this.blue, this.color.getBlue());
+    }
+
+    @Test
+    @Tags({
+        @Tag("getter"),
+        @Tag("equals")
+    })
+    public void testGetHexValueEquals() {
+        assertEquals(this.hexValue, this.color.getHexValue());
+    }
+
+    /*
+        Setters
+     */
+
+    @Test
+    @Tags({
+        @Tag("setter"),
+        @Tag("equals")
+    })
+    public void testSetRedEquals() {
+        int color = 26;
+        this.color.setRed(color);
+        assertEquals(color, this.color.getRed());
+        assertAll("Color object non conformes",
+            ()->assertEquals(color, this.color.getRed(), "getRed() est incorrect"),
+            ()->assertEquals(this.green, this.color.getGreen(), "getGreen() est incorrect"),
+            ()->assertEquals(this.blue, this.color.getBlue(), "getBlue() est incorrect"),
+            ()->assertEquals(this.getHexValue(color, this.green, this.blue), this.color.getHexValue(), "getHexValue() est incorrect")
+        );
+    }
+
+    @Test
+    @Tags({
+        @Tag("setter"),
+        @Tag("equals")
+    })
+    public void testSetGreenEquals() {
+        int color = 26;
+        this.color.setGreen(color);
+        assertAll("Color object non conformes",
+                ()->assertEquals(this.red, this.color.getRed(), "getRed() est incorrect"),
+                ()->assertEquals(color, this.color.getGreen(), "getGreen() est incorrect"),
+                ()->assertEquals(this.blue, this.color.getBlue(), "getBlue() est incorrect"),
+                ()->assertEquals(this.getHexValue(this.red, color, this.blue), this.color.getHexValue(), "getHexValue() est incorrect")
+        );
+
+    }
+
+    @Test
+    @Tags({
+        @Tag("setter"),
+        @Tag("equals")
+    })
+    public void testSetBlueEquals() {
+        int color = 26;
+        this.color.setBlue(color);
+        assertAll("Color object non conformes",
+                ()->assertEquals(this.red, this.color.getRed(), "getRed() est incorrect"),
+                ()->assertEquals(this.green, this.color.getGreen(), "getGreen() est incorrect"),
+                ()->assertEquals(color, this.color.getBlue(), "getBlue() est incorrect"),
+                ()->assertEquals(this.getHexValue(this.red, this.green, color), this.color.getHexValue(), "getHexValue() est incorrect")
+        );
+    }
+
+    @Test
+    @Tags({
+        @Tag("setter"),
+        @Tag("equals")
+    })
+    public void testSetHexValueEquals() {
+        String hex = "#33CCFF";
+        this.color.setHexValue(hex);
+        assertAll("Color object non conformes",
+                ()->assertEquals(Integer.valueOf(hex.substring(1, 3), 16), this.color.getRed(), "getRed() est incorrect"),
+                ()->assertEquals(Integer.valueOf(hex.substring(3, 5), 16), this.color.getGreen(), "getGreen() est incorrect"),
+                ()->assertEquals(Integer.valueOf(hex.substring(5, 7), 16), this.color.getBlue(), "getBlue() est incorrect"),
+                ()->assertEquals(hex, this.color.getHexValue(), "getHexValue() est incorrect")
+        );
+    }
+
+    // TODO => exceptions
+
+    /*
+        toString
+     */
+
+    @Test
+    @Tags({
+        @Tag("toString"),
+        @Tag("equals")
+    })
+    public void testToStringEquals() {
+        String expected = "[value=" + this.hexValue + ", r=" + this.red + ", g=" + this.green + ", b=" + this.blue + "]";
+        assertEquals(expected, this.color.toString());
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        this.color = null;
+    }
 }
